7d90c220765c1c862f3d5720dacdb05697b76a76
nakandev <nakandev.s@gmail.com>
2024-01-18 08:55:47 +0900

    update emulation test to check speed

--- sw/emulator/test/test_00_sim/main_cui.cpp
+++ sw/emulator/test/test_00_sim/main_cui.cpp
@@ -60,9 +60,44 @@ int main_emulator(int argc, char* argv[])
   return 0;
 }
 
+int main_speed(int argc, char* argv[])
+{
+  using Duration = microseconds;
+  vector<uint64_t> times;
+  string path = "/home/nyalry/nakan/dev/hobby/game-console/sw/dev/c/tutorial/mw/00_template/tutorial_mw_00_template.exe";
+  if (argc == 2) {
+    path = string(argv[1]);
+  }
+  auto board = Board();
+
+  // debugLevel = 1;
+  board.cpu.setMaxCycles(1'000'000'000);
+  board.cpu.loadElf(path);
+
+  using namespace std;
+  using namespace std::chrono;
+  using dsec = duration<double>;
+  auto msecPerFrame = microseconds(16666);
+  auto beginTime = high_resolution_clock::now();
+  for (auto i = 0; i < 1'000'000; ++i)
+  {
+    board.cpu.stepCycle();
+  }
+  auto endTime = high_resolution_clock::now();
+  auto procTime = duration_cast<microseconds>(endTime - beginTime);
+  fmt::print("procTime = {}\n", procTime.count());
+  fmt::print("instr = {}\n", board.cpu.getInstrCount());
+  fmt::print("cycle = {}\n", board.cpu.getCycleCount());
+  double speed = board.cpu.getInstrCount() / (double)procTime.count();
+  fmt::print("speed(instr/proc) = {:.2f}\n", speed);
+
+  return 0;
+}
+
 int main(int argc, char* argv[])
 {
+  main_speed(argc, argv);
   // main_emulator(argc, argv);
-  main_disassembler(argc, argv);
+  // main_disassembler(argc, argv);
   return 0;
 }
