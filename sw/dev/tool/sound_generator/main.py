396b84ec35d68fb77b7acb0a189977f1e0639352
nakandev <nakandev.s@gmail.com>
2024-03-11 04:28:48 +0900

    update sound generator tool

--- sw/dev/tool/sound_generator/main.py
+++ sw/dev/tool/sound_generator/main.py
@@ -7,6 +7,8 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import PySimpleGUI as sg
 import matplotlib
 import sounddevice as sd
+import os
+import json
 
 
 matplotlib.use('TkAgg')
@@ -136,6 +138,29 @@ class SignalModuleGadget(SignalModule):
             float(values[f'{name}_env_R_slider']),
         )
 
+    def get_json(self):
+        return {
+            'fn': self.fn,
+            'amp': self.amp,
+            'freq': self.freq,
+            'arg': self.arg,
+            'At': self.At,
+            'Dt': self.Dt,
+            'Slv': self.Slv,
+            'Rt': self.Rt,
+        }
+
+    def set_json(self, window, jvalues):
+        name = self.name
+        self.set(*list(jvalues.values()))
+        window[f'{name}_sig_amp_slider'].Update(jvalues['amp'])
+        window[f'{name}_sig_freq_slider'].Update(jvalues['freq'])
+        window[f'{name}_sig_arg_slider'].Update(jvalues['arg'])
+        window[f'{name}_env_A_slider'].Update(jvalues['At'])
+        window[f'{name}_env_D_slider'].Update(jvalues['Dt'])
+        window[f'{name}_env_S_slider'].Update(jvalues['Slv'])
+        window[f'{name}_env_R_slider'].Update(jvalues['Rt'])
+
     def handle_event(self, event, values, window):
         name = self.name
         if event is None:
@@ -198,6 +223,12 @@ class SignalModuleGadget(SignalModule):
         self.env_fig_canvas_agg = draw_figure(window[f'{name}_env_canvas'].TKCanvas, self.env_fig)
 
 
+def algo_graph_update(algo):
+    algo_image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), f'algo{algo}.png')
+    algo_graph.draw_image(filename=algo_image_path, location=(0, 120))
+    algo_graph.update()
+
+
 # ------------------------------- PASTE YOUR MATPLOTLIB CODE HERE -------------------------------
 #
 # # Goal is to have your plot contained in the variable  "fig"
@@ -210,14 +241,14 @@ t_fin = 2.0  # 収録終了時刻 [s] (任意)
 dt = 1 / f_s  # サンプリング周期 dt[s]
 N = int(f_s * t_fin)  # サンプル数 [個]
 
-fft_fig = matplotlib.figure.Figure(figsize=(8, 2), dpi=80)
+fft_fig = matplotlib.figure.Figure(figsize=(8, 3), dpi=80)
 fft_subplot = fft_fig.add_subplot(111)
-wave_fig = matplotlib.figure.Figure(figsize=(8, 2), dpi=80)
+wave_fig = matplotlib.figure.Figure(figsize=(8, 3), dpi=80)
 wave_subplot = wave_fig.add_subplot(111)
-env1_fig = matplotlib.figure.Figure(figsize=(8, 2), dpi=80)
-env1_subplot = env1_fig.add_subplot(111)
-env2_fig = matplotlib.figure.Figure(figsize=(8, 2), dpi=80)
-env2_subplot = env2_fig.add_subplot(111)
+# env1_fig = matplotlib.figure.Figure(figsize=(8, 2), dpi=80)
+# env1_subplot = env1_fig.add_subplot(111)
+# env2_fig = matplotlib.figure.Figure(figsize=(8, 2), dpi=80)
+# env2_subplot = env2_fig.add_subplot(111)
 t = np.arange(0, t_fin, dt)
 
 def wave(a, b, fc, fm):
@@ -240,10 +271,10 @@ def draw_figure(canvas, figure):
 
 
 # ------------------------------- Beginning of GUI CODE -------------------------------
-sigmod1_gadget = SignalModuleGadget('signal1')
-sigmod2_gadget = SignalModuleGadget('signal2')
-sigmod3_gadget = SignalModuleGadget('signal3')
-sigmod4_gadget = SignalModuleGadget('signal4')
+sigmod1_gadget = SignalModuleGadget('signal0')
+sigmod2_gadget = SignalModuleGadget('signal1')
+sigmod3_gadget = SignalModuleGadget('signal2')
+sigmod4_gadget = SignalModuleGadget('signal3')
 sigmod1_frame = sigmod1_gadget.create()
 sigmod2_frame = sigmod2_gadget.create()
 sigmod3_frame = sigmod3_gadget.create()
@@ -263,9 +294,20 @@ algo_radios = [
 
 manip_layout = sg.Column([
     algo_radios,
+    [sg.Graph(
+        key='algo_graph',
+        canvas_size=(140, 120), graph_bottom_left=(0, 0), graph_top_right=(140, 120),
+        background_color='white',
+    )],
     [sigmod1_frame, sigmod2_frame],
     [sigmod3_frame, sigmod4_frame],
-    [sg.Button('Play', key='play')]],
+    # [sg.Button('Play', key='play')],
+    [
+        sg.Button('Play', key='play'),
+        # sg.Button('Save', key='save'),
+        sg.FileSaveAs('Save As', key='saveas', target='saveas', enable_events=True),
+        sg.FileBrowse('Load', key='load', target='load', enable_events=True),
+    ]],
     vertical_alignment='top')
 fft_canvas = sg.Canvas(key='fft_canvas')
 wave_canvas = sg.Canvas(key='wave_canvas')
@@ -295,10 +337,19 @@ sigmod2_gadget.setup_env_graph(window)
 sigmod3_gadget.setup_env_graph(window)
 sigmod4_gadget.setup_env_graph(window)
 
+algo = 0
+algo_image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), f'algo{algo}.png')
+algo_graph = window['algo_graph']
+algo_graph.draw_image(filename=algo_image_path, location=(0, 120))
+algo_graph.update()
+
+json_path = None
+
 while True:
     event, values = window.read()
     if event is None:
         break
+    # algo = 0
     if any([event == f'algo{i}' for i in range(8)]):
         for i in range(8):
             if values[f'algo{i}']:
@@ -326,7 +377,8 @@ while True:
         y2 = k2 * y2
         y1 = sigmod1_gadget.signal(t, y2)
         k1 = sigmod1_gadget.envelope(t)
-        y = k1 * y1
+        y1 = k1 * y1
+        y = y1
     elif algo == 1:
         y4 = sigmod4_gadget.signal(t)
         k4 = sigmod4_gadget.envelope(t)
@@ -339,6 +391,7 @@ while True:
         y2 = k2 * y2
         y1 = sigmod1_gadget.signal(t, y2)
         k1 = sigmod1_gadget.envelope(t)
+        y1 = k1 * y1
         y = y1
     elif algo == 2:
         y4 = sigmod4_gadget.signal(t)
@@ -352,6 +405,7 @@ while True:
         y2 = k2 * y2
         y1 = sigmod1_gadget.signal(t, y4 + y2)
         k1 = sigmod1_gadget.envelope(t)
+        y1 = k1 * y1
         y = y1
     elif algo == 3:
         y4 = sigmod4_gadget.signal(t)
@@ -365,6 +419,7 @@ while True:
         y2 = k2 * y2
         y1 = sigmod1_gadget.signal(t, y3 + y2)
         k1 = sigmod1_gadget.envelope(t)
+        y1 = k1 * y1
         y = y1
     elif algo == 4:
         y4 = sigmod4_gadget.signal(t)
@@ -378,6 +433,7 @@ while True:
         y2 = k2 * y2
         y1 = sigmod1_gadget.signal(t, y2)
         k1 = sigmod1_gadget.envelope(t)
+        y1 = k1 * y1
         y = y3 + y1
     elif algo == 5:
         y4 = sigmod4_gadget.signal(t)
@@ -391,6 +447,7 @@ while True:
         y2 = k2 * y2
         y1 = sigmod1_gadget.signal(t, y4)
         k1 = sigmod1_gadget.envelope(t)
+        y1 = k1 * y1
         y = y3 + y2 + y1
     elif algo == 6:
         y4 = sigmod4_gadget.signal(t)
@@ -404,6 +461,7 @@ while True:
         y2 = k2 * y2
         y1 = sigmod1_gadget.signal(t)
         k1 = sigmod1_gadget.envelope(t)
+        y1 = k1 * y1
         y = y3 + y2 + y1
     elif algo == 7:
         y4 = sigmod4_gadget.signal(t)
@@ -417,6 +475,7 @@ while True:
         y2 = k2 * y2
         y1 = sigmod1_gadget.signal(t)
         k1 = sigmod1_gadget.envelope(t)
+        y1 = k1 * y1
         y = y4 + y3 + y2 + y1
 
     y_fft = np.fft.fft(y)
@@ -439,6 +498,8 @@ while True:
     # for i in range(len(y)):
     #     env_y[i] = k1[i] * y[i]
 
+    algo_graph_update(algo)
+
     if event is None:
         break
     elif event == 'fc_btn':
@@ -447,5 +508,52 @@ while True:
         window['fm_slider'].Update(values['fm_txt'])
     elif event == 'play':
         sd.play(env_y, N)
+    elif event == 'saveas':
+        if not json_path:
+            break
+        sigs_json = {
+            'algo': algo,
+            'signals': [
+                sigmod1_gadget.get_json(),
+                sigmod2_gadget.get_json(),
+                sigmod3_gadget.get_json(),
+                sigmod4_gadget.get_json(),
+            ],
+        }
+        json_path = values['saveas']
+        with open(json_path, 'w') as f:
+            f.write(json.dumps(sigs_json))
+    elif event == 'save':
+        sigs_json = {
+            'algo': algo,
+            'signals': [
+                sigmod1_gadget.get_json(),
+                sigmod2_gadget.get_json(),
+                sigmod3_gadget.get_json(),
+                sigmod4_gadget.get_json(),
+            ],
+        }
+        with open(json_path, 'w') as f:
+            f.write(json.dumps(sigs_json))
+    elif event == 'load':
+        try:
+            sigs_json = None
+            with open(values['load'], 'r') as f:
+                sigs_json = json.load(f)
+            json_path = values['load']
+            algo = sigs_json['algo']
+            for i in range(8):
+                window[f'algo{i}'].Update(False)
+                if i == algo:
+                    window[f'algo{i}'].Update(True)
+            sigmod1_gadget.set_json(window, sigs_json['signals'][0])
+            sigmod2_gadget.set_json(window, sigs_json['signals'][1])
+            sigmod3_gadget.set_json(window, sigs_json['signals'][2])
+            sigmod4_gadget.set_json(window, sigs_json['signals'][3])
+            algo_graph_update(algo)
+        except Exception as e:
+            print(e)
+            pass
+
 
 window.close()
